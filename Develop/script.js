e26637100641e61d1d1f3b1131d0dbb800f9d719
sepan mohammed <sepanmustafa@gmail.com>
2023-09-22 06:48:47 -0500

    added if statement to check for current time

--- Develop/script.js
+++ Develop/script.js
@@ -27,7 +27,12 @@ $ ('.btn').on('click', function () {
 
   timeBlockElms.each(function() {
     var currentHour = $(this).parent().attr('id');
-    
+    var fromTime = dayjs().hour();
+    if (currentHour === fromTime) {
+      console.log('true');
+    } else {
+      console.log('false');
+    }
   });
 
   // TODO: Add code to get any user input that was saved in localStorage and set
@@ -44,6 +49,16 @@ function displayTime () {
 
 $("#9 .description").val(localStorage.getItem('9'));
 $("#10 .description").val(localStorage.getItem('10'));
+$("#11 .description").val(localStorage.getItem('11'));
+$("#12 .description").val(localStorage.getItem('12'));
+$("#13 .description").val(localStorage.getItem('13'));
+$("#14 .description").val(localStorage.getItem('14'));
+$("#15 .description").val(localStorage.getItem('15'));
+$("#16 .description").val(localStorage.getItem('16'));
+$("#17 .description").val(localStorage.getItem('17'));
+$("#18 .description").val(localStorage.getItem('18'));
+$("#19 .description").val(localStorage.getItem('19'));
+$("#20 .description").val(localStorage.getItem('20'));
 
 
 
